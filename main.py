bcdd546a25d8a695e6d19e63fcdfda539818303a
anton <anton@Antons-MacBook-Pro.local>
2025-11-09 23:13:39 +0200

    remove chatt tts

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from services.music import createSet
-# from llm_models.tts import record
 
 
 def main():
-    model = input("Music-1,Stories-2: ").strip()
-
-    if model == "1":
-        createSet()
-    # elif model == "2":
-        # record()
+    createSet()
 
 
 if __name__ == "__main__":
